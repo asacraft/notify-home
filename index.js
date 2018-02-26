c77ab3850b2400c14b4903bf9aac474bfe3f2c07
asacraft <asacraftn@gmail.com>
2018-02-26 17:08:08 +0900

    聞き直し機能を追加

--- index.js
+++ index.js
@@ -8,18 +8,45 @@ const transport = {
   type: 'mqtt',
   token: process.env.BEEBOTTE_CHANNEL_TOKEN
 };
+const names = {
+  dad: 'お父さん',
+  mon: 'お母さん'
+};
 
 googlehome.device(process.env.DEVICE_NAME, process.env.LANG);
 
 const client = new bbt.Stream({ transport: transport });
 client.on('connected', function () {
-  client.subscribe(channel, 'dad', handleEvent('お父さん'));
-  client.subscribe(channel, 'mom', handleEvent('お母さん'));
+  client.subscribe(channel, 'dad', handleEvent('dad'));
+  client.subscribe(channel, 'mom', handleEvent('mon'));
+  client.subscribe(channel, 'repeatdad', handleRepeat('dad'));
+  client.subscribe(channel, 'repeatmom', handleRepeat('mom'));
 });
 
 function handleEvent(resource) {
   return function (message) {
-    googlehome.notify(resource + 'から、' + message.data, res => console.log(res));
+    notify(resource, message.data);
   };
+}
+
+function notify(resource, message) {
+  googlehome.notify(names[resource] + 'から、' + message, res => console.log(res));
+}
+
+const reader = new bbt.Connector({ token: process.env.BEEBOTTE_CHANNEL_TOKEN });
+
+function handleRepeat(resource) {
+  return function(message) {
+    reader.read(
+      { channel: channel, resource: resource, limit: 1 },
+      function (err, res) {
+        if (err) throw(err);
+	if (res.length > 0) {
+	  notify(resource, res[0].data);
+	}
+	console.log(res);
+      }
+    );
+  }
 }
 
